844e62c51e967407350a1160830d8bcae1391a8d
Michał Kosior @crqch <48185508+crqch@users.noreply.github.com>
2026-06-29 12:39:16 +0200

    feat: add lexer tests to main

--- src/main.zig
+++ src/main.zig
@@ -22,10 +22,6 @@ pub fn main(init: std.process.Init) !void {
     }
 }
 
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+test "run all tests" {
+    _ = @import("lexer.zig");
 }
