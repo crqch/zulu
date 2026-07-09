267bca15cea6867b9c7ed6095ac3d1e7a3c8d496
Michał Kosior @crqch <48185508+crqch@users.noreply.github.com>
2026-07-09 15:31:49 +0200

    feat: add ansi struct and improve tests logging

--- src/root.zig
+++ src/root.zig
@@ -7,6 +7,17 @@ pub const Lexer = @import("./lexer.zig").Lexer;
 pub const AstPrinter = @import("./ast.zig").AstPrinter;
 pub const Interpreter = @import("./interpreter.zig");
 
+pub const ansi = struct {
+    pub const reset = "\x1b[0m";
+    pub const bold = "\x1b[1m";
+    pub const red = "\x1b[31m";
+    pub const green = "\x1b[32m";
+    pub const yellow = "\x1b[33m";
+    pub const blue = "\x1b[34m";
+    pub const cyan = "\x1b[36m";
+    pub const gray = "\x1b[90m";
+};
+
 test "run all tests" {
     _ = @import("./lexer.zig");
 }
